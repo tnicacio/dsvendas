a3d633435161a0538324b9b23d892c549c171944
tnicacio <nicacio.t92@gmail.com>
2021-05-03 21:11:01 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => (
+  <footer className="footer mt-auto py-3 bg-dark">
+    <div className="container">
+      <p className="text-light">App desenvolvido por <a href="https://github.com/tnicacio" target="_blank" rel="noreferrer">Tiago Nicácio</a></p>
+      <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+    </div>
+  </footer>
+);
